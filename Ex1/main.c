1bf36b0870fc495de50d1fe55e51f6249e06f934
Bernardo Esteves <esteveste@users.noreply.github.com>
2017-09-28 13:23:41 +0100

    Mudar cenas, + Ex1 + parses

---
+++ Ex1/main.c
@@ -0,0 +1,128 @@
+/*
+// Projeto SO - exercicio 1, version 03
+// Sistemas Operativos, DEI/IST/ULisboa 2017-18
+//Grupo 16
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "matrix2d.h"
+
+#include "mplib3.h"
+
+/*--------------------------------------------------------------------
+| Function: simul
+---------------------------------------------------------------------*/
+
+DoubleMatrix2D *simul(DoubleMatrix2D *matrix, DoubleMatrix2D *matrix_aux, int linhas, int colunas, int numIteracoes) {
+
+  int n,i,j = 0;
+  DoubleMatrix2D *temp;
+
+  for (int n = 0; n < numIteracoes; ++n)
+  {
+
+    for (int i = 1; i < linhas-1; ++i)
+      for (int j = 1; j < colunas-1; ++j)
+        dm2dSetEntry(matrix_aux,i,j,(dm2dGetEntry(matrix,i-1,j) + dm2dGetEntry(matrix,i+1,j) + dm2dGetEntry(matrix,i,j-1) + dm2dGetEntry(matrix,i,j+1))/4);
+    
+    temp = matrix;
+    matrix = matrix_aux;
+    matrix_aux = temp;
+
+  }
+    return matrix;
+}
+
+/*--------------------------------------------------------------------
+| Function: parse_integer_or_exit
+---------------------------------------------------------------------*/
+
+int parse_integer_or_exit(char const *str, char const *name)
+{
+  int value;
+ 
+  if(sscanf(str, "%d", &value) != 1) {
+    fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
+    exit(1);
+  }
+  return value;
+}
+
+/*--------------------------------------------------------------------
+| Function: parse_double_or_exit
+---------------------------------------------------------------------*/
+
+double parse_double_or_exit(char const *str, char const *name)
+{
+  double value;
+
+  if(sscanf(str, "%lf", &value) != 1) {
+    fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
+    exit(1);
+  }
+  return value;
+}
+
+
+/*--------------------------------------------------------------------
+| Function: main
+---------------------------------------------------------------------*/
+
+int main (int argc, char** argv) {
+
+  if(argc != 9) {
+    fprintf(stderr, "\nNumero invalido de argumentos.\n");
+    fprintf(stderr, "Uso: heatSim N tEsq tSup tDir tInf iteracoes trab csz\n\n");
+    return 1;
+  }
+
+  /* argv[0] = program name */
+  int N = parse_integer_or_exit(argv[1], "N");
+  double tEsq = parse_double_or_exit(argv[2], "tEsq");
+  double tSup = parse_double_or_exit(argv[3], "tSup");
+  double tDir = parse_double_or_exit(argv[4], "tDir");
+  double tInf = parse_double_or_exit(argv[5], "tInf");
+  int iteracoes = parse_integer_or_exit(argv[6], "iteracoes");
+  int trab = parse_integer_or_exit(argv[7], "trab");
+  int csz = parse_integer_or_exit(argv[8], "csz");
+
+  if(N<1 || tEsq < 0 || tSup < 0 || tDir < 0|| tInf < 0 || iteracoes < 1){
+    fprintf(stderr,"Contexto dos argumentos invalido\n");
+    return 1;
+  }
+
+
+  DoubleMatrix2D *matrix, *matrix_aux, *result;
+
+
+  fprintf(stderr, "\nArgumentos:\n"
+	" N=%d tEsq=%.1f tSup=%.1f tDir=%.1f tInf=%.1f iteracoes=%d\n",
+	N, tEsq, tSup, tDir, tInf, iteracoes);
+
+
+  matrix = dm2dNew(N+2, N+2);
+  matrix_aux = dm2dNew(N+2, N+2);
+
+
+  /* FAZER ALTERACOES AQUI */
+
+
+  dm2dSetLineTo (matrix, 0, tSup);
+  dm2dSetLineTo (matrix, N+1, tInf);
+  dm2dSetColumnTo (matrix, 0, tEsq);
+  dm2dSetColumnTo (matrix, N+1, tDir);
+
+  dm2dCopy (matrix_aux, matrix);
+
+  result = simul(matrix, matrix_aux, N+2, N+2, iteracoes);
+
+  dm2dPrint(result);
+
+  dm2dFree(matrix);
+  dm2dFree(matrix_aux);
+
+  return 0;
+}
